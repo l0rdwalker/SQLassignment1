d7a5693b0ea91047f22fd6e1e9d581bba6e4ab9c
wwal7356 <wwal7356@uni.sydney.edu.au>
2023-08-13 14:34:06 +1000

    Basic tables

--- epic.sql
+++ epic.sql
@@ -1,6 +1,31 @@
-FROM epic SELECT *
-WHERE epicness = 100
+CREATE TABLE Student (
+  StudentID INTEGER NOT NULL,
+  StudentName VARCHAR(20) NOT NULL,
+  Email VARCHAR(50),
+  PRIMARY KEY (StudentID)
+);
+
+CREATE TABLE Meeting (
+    StartTime DATETIME NOT NULL,
+    Duration int not NULL
+);/*Maybe change Duration to EndTime?s*/
+
+CREATE TABLE Competition (
+    EndDate DATETIME NOT NULL,
+    Prize VARCHAR(250)
+);
 
 
+CREATE TABLE Club (
+    ClubName VARCHAR(20),
+    Abbreviation VARCHAR(5),
+    AGMdate DATETIME,
+    PRIMARY KEY (ClubName)
+);
 
-pogness
+CREATE TABLE Ac1vity (
+    AName VARCHAR(15),
+    Descrip1on VARCHAR(250),
+    Budget INTEGER,
+    PRIMARY KEY (AName)
+);
