8d8896470ea7017e2254de5ef80947b5fe30b681
OrphixG <110904093+OrphixG@users.noreply.github.com>
2023-08-13 13:45:52 +0800

    Updated Code

--- epic.sql
+++ epic.sql
@@ -1,36 +1,36 @@
+CREATE TABLE Activity (
+    AName VARCHAR(50) NOT NULL,
+    Description VARCHAR(1000),
+    Budget INTEGER,
+    PRIMARY KEY (AName)
+);
+
 CREATE TABLE Student (
   StudentID INTEGER NOT NULL,
-  StudentName VARCHAR(20) NOT NULL,
+  StudentName VARCHAR(50) NOT NULL,
   Email VARCHAR(50),
   PRIMARY KEY (StudentID)
 );
 
 CREATE TABLE Meeting (
-    StartTime DATETIME NOT NULL,
-    Duration int not NULL
+    StartTime TIMESTAMP NOT NULL,
+    Duration TIME NOT NULL
 );/*Maybe change Duration to EndTime?s*/
+
+CREATE TABLE Competition (
+    EndDate TIMESTAMP NOT NULL,
+    Prize VARCHAR(250)
+);
+
+CREATE TABLE Club (
+    ClubName VARCHAR(50) NOT NULL,
+    Abbreviation VARCHAR(10),
+    AGMDate TIMESTAMP,
+    PRIMARY KEY (ClubName)
+);
 
 CREATE TABLE Contact (
     StudentID INTEGER NOT NULL,
     Email VARCHAR(50)
     FOREIGN KEY (StudentID) REFERENCES Student(StudentID)
 )
-
-CREATE TABLE Competition (
-    EndDate DATETIME NOT NULL,
-    Prize VARCHAR(250)
-);
-
-CREATE TABLE Club (
-    ClubName VARCHAR(20),
-    Abbreviation VARCHAR(5),
-    AGMdate DATETIME,
-    PRIMARY KEY (ClubName)
-);
-
-CREATE TABLE Ac1vity (
-    AName VARCHAR(15),
-    Descrip1on VARCHAR(250),
-    Budget INTEGER,
-    PRIMARY KEY (AName)
-);
