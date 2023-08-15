31d6e582d6cc931898e3e3b4df2fd5a10c2f7ebe
wwal7356 <wwal7356@uni.sydney.edu.au>
2023-08-15 15:00:39 +1000

    Updated with some basic relationships

--- epic.sql
+++ epic.sql
@@ -1,3 +1,32 @@
+CREATE TABLE Student (
+  StudentID INTEGER NOT NULL,
+  StudentName VARCHAR(50) NOT NULL,
+  Email VARCHAR(50),
+  PRIMARY KEY (StudentID)
+);
+CREATE TABLE Club (
+    ClubName VARCHAR(50) NOT NULL,
+    Abbreviation VARCHAR(10),
+    AGMDate TIMESTAMP,
+    PRIMARY KEY (ClubName)
+);
+
+CREATE TABLE PresidentOF (
+    Student INTEGER,
+    Club VARCHAR(50),
+    FOREIGN KEY (Student) REFERENCES Student(StudentID), 
+    PRIMARY KEY (Club)
+);
+
+CREATE TABLE BelongsTo (
+    Student INTEGER,
+    Club VARCHAR(50),
+    JoinDate DATE NOT NULL,
+    FOREIGN KEY (Student) REFERENCES Student(StudentID), 
+    FOREIGN KEY (Club) REFERENCES Club(ClubName),
+    PRIMARY KEY (Student,Club)
+);
+
 CREATE TABLE Activity (
     AName VARCHAR(50) NOT NULL,
     Description VARCHAR(1000),
@@ -5,19 +34,34 @@ CREATE TABLE Activity (
     PRIMARY KEY (AName)
 );
 
-CREATE TABLE Student (
-  StudentID INTEGER NOT NULL,
-  StudentName VARCHAR(50) NOT NULL,
-  Email VARCHAR(50),
-  PRIMARY KEY (StudentID)
+CREATE TABLE WinnerOf (
+    CompID VARCHAR(50),
+    StudentID INTEGER NOT NULL,
+    PRIMARY KEY (CompID),
+    FOREIGN KEY (StudentID) REFERENCES Student(StudentID)
+);
+
+CREATE TABLE RunBy(
+    ClubID VARCHAR(50),
+    ActivityID VARCHAR(50),
+    PRIMARY KEY (ActivityID),
+    FOREIGN KEY (ClubID) REFERENCES Club(ClubName), 
+    FOREIGN KEY (ActivityID) REFERENCES Activity(AName)
+);
+
+CREATE TABLE TakesPart (
+    StudentID INTEGER,
+    ClubName VARCHAR(50),
+    PRIMARY KEY (StudentID,ClubName),
+    FOREIGN KEY (StudentID) REFERENCES Student(StudentID), 
+    FOREIGN KEY (ClubName) REFERENCES Student(ClubName)
 );
 
 CREATE TABLE Meeting (
     StartTime TIMESTAMP NOT NULL,
     Duration TIME NOT NULL,
     MeetingID VARCHAR(50) NOT NULL,
-    FOREIGN KEY (MeetingID) REFERENCES Activity(AName),
-    PRIMARY KEY (MeetingID)
+    FOREIGN KEY (MeetingID) REFERENCES Activity(AName)
 );
 
 CREATE TABLE Competition (
@@ -28,30 +72,11 @@ CREATE TABLE Competition (
     PRIMARY KEY (CompID)
 );
 
-CREATE TABLE idList(
-    idofIDList
-    StudentID INTEGER NOT NULL,
-    ClubID VARCHAR(50),
-
-    FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
-    FOREIGN KEY (ClubID) REFERENCES Club(ClubName)
-);
-
-CREATE TABLE Club (
-    ClubName VARCHAR(50) NOT NULL,
-    Abbreviation VARCHAR(10),
-    AGMDate TIMESTAMP,
-    PRIMARY KEY (ClubName)'
-    '
-);
-
 CREATE TABLE Contact (
     StudentID INTEGER NOT NULL,
     Email VARCHAR(50),
     FOREIGN KEY (StudentID) REFERENCES Student(StudentID)
 );
-
-
 
 DROP TABLE IF EXISTS Contact;
 DROP TABLE IF EXISTS Competition;
