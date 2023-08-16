ad7131c81eb21e68d383ea5a0f4dd2dc7d1f97c2
wwal7356 <wwal7356@uni.sydney.edu.au>
2023-08-16 20:51:03 +1000

    more fixes

--- epic.sql
+++ epic.sql
@@ -8,17 +8,19 @@ BEGIN
     END LOOP;
 END $$;
 
-
 CREATE TABLE Student (
   StudentID INTEGER NOT NULL,
   StudentName VARCHAR(50) NOT NULL,
   Email VARCHAR(50),
   PRIMARY KEY (StudentID)
 );
+
 CREATE TABLE Club (
     ClubName VARCHAR(50) NOT NULL,
     Abbreviation VARCHAR(10),
     AGMDate TIMESTAMP,
+    PresidentId INTEGER NOT NULL,
+    FOREIGN KEY (PresidentId) REFERENCES Student(StudentID),
     PRIMARY KEY (ClubName)
 );
 
@@ -29,43 +31,11 @@ CREATE TABLE PresidentOF (
     PRIMARY KEY (Club)
 );
 
-CREATE TABLE BelongsTo (
-    Student INTEGER,
-    ClubName VARCHAR(50),
-    JoinDate DATE NOT NULL,
-    FOREIGN KEY (Student) REFERENCES Student(StudentID), 
-    FOREIGN KEY (ClubName) REFERENCES Club(ClubName),
-    PRIMARY KEY (Student,ClubName)
-);
-
 CREATE TABLE Activity (
     AName VARCHAR(50) NOT NULL,
     Description VARCHAR(1000),
     Budget INTEGER,
     PRIMARY KEY (AName)
-);
-
-CREATE TABLE WinnerOf (
-    CompID VARCHAR(50),
-    StudentID INTEGER NOT NULL,
-    PRIMARY KEY (CompID),
-    FOREIGN KEY (StudentID) REFERENCES Student(StudentID)
-);
-
-CREATE TABLE RunBy(
-    ClubID VARCHAR(50),
-    ActivityID VARCHAR(50),
-    PRIMARY KEY (ActivityID),
-    FOREIGN KEY (ClubID) REFERENCES Club(ClubName), 
-    FOREIGN KEY (ActivityID) REFERENCES Activity(AName)
-);
-
-CREATE TABLE TakesPart (
-    StudentID INTEGER,
-    ClubName VARCHAR(50),
-    PRIMARY KEY (StudentID,ClubName),
-    FOREIGN KEY (StudentID) REFERENCES Student(StudentID), 
-    FOREIGN KEY (ClubName) REFERENCES Club(ClubName)
 );
 
 CREATE TABLE Meeting (
@@ -83,15 +53,6 @@ CREATE TABLE Competition (
     PRIMARY KEY (CompName)
 );
 
-CREATE TABLE Club (
-    ClubName VARCHAR(50) NOT NULL,
-    Abbreviation VARCHAR(10),
-    AGMDate TIMESTAMP,
-    PresidentId INTEGER NOT NULL,
-    FOREIGN KEY (PresidentId) REFERENCES Student(StudentID),
-    PRIMARY KEY (ClubName)
-);
-
 CREATE TABLE Contact (
     StudentID INTEGER NOT NULL,
     Email VARCHAR(50),
@@ -99,7 +60,6 @@ CREATE TABLE Contact (
 );
 
 -- relationships --
-
 CREATE TABLE RunBy (
     ClubName VARCHAR(50) NOT NULL,
     AName VARCHAR(50) NOT NULL,
@@ -112,7 +72,7 @@ CREATE TABLE BelongsTo (
     StudentID INTEGER NOT NULL,
     ClubName VARCHAR(50) NOT NULL,
     FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
-    FOREIGN KEY (ClubName) REFERENCES Activity(ClubName),
+    FOREIGN KEY (ClubName) REFERENCES Club(ClubName),
     PRIMARY KEY (StudentID, ClubName)
 );
 
@@ -129,7 +89,7 @@ CREATE TABLE WinnerOf(
     StudentID INTEGER NOT NULL,     -- null or not null? cuz there could be 0 winner
     FOREIGN KEY (CompName) REFERENCES Competition(CompName),
     FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
-    PRIMARY KEY (Competition)
+    PRIMARY KEY (CompName)
 );
 
 -- CREATE TABLE idList(
@@ -141,16 +101,7 @@ CREATE TABLE WinnerOf(
 --     FOREIGN KEY (ClubID) REFERENCES Club(ClubName)
 -- );
 
-DROP TABLE IF EXISTS Contact;
-DROP TABLE IF EXISTS Competition;
-DROP TABLE IF EXISTS Meeting;
-DROP TABLE IF EXISTS Student;
-DROP TABLE IF EXISTS Activity;
-DROP TABLE IF EXISTS Club;
-DROP TABLE IF EXISTS idList;
-
 SELECT table_name
 FROM information_schema.tables
 WHERE table_schema = 'public' -- You might need to adjust the schema name if it's different
 AND table_type = 'BASE TABLE';
-
---
+++ testing.sql
@@ -0,0 +1,4 @@
+SELECT * FROM student;
+
+
+--INSERT INTO UnitOfStudy VALUES (99887766,'Computer Essentials',6,11223344);
